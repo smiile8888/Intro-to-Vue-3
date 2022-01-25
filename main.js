8cd0ec45eb9860dd498df89b28128bf2b5a9847c
Smile Thanapattheerakul <smiile@Smiles-MacBook-Pro.local>
2022-01-24 22:46:20 -0500

    Complete challenge list rendering (v-for)

--- main.js
+++ main.js
@@ -4,7 +4,12 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: '2234', color: 'Blue' },
+                { id: '2235', color: 'Green' }
+            ],
+            sizes: ['S/P', 'L', 'XL']
         }
     }
 })
